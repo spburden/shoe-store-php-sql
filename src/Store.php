805361927e2ab7e9cf3467f005a87d1554d7fe80
Stephen Burden <spburden@hotmail.com>
2016-09-30 09:48:50 -0700

    add basic functions for store class

--- src/Store.php
+++ src/Store.php
@@ -48,8 +48,8 @@
 
         static function deleteAll()
         {
-            $GLOBALS['DB']->exec("DELETE FROM shoes;");
             $GLOBALS['DB']->exec("DELETE FROM stores;");
+            $GLOBALS['DB']->exec("DELETE FROM stores_brands WHERE store_id = {$this->getId()};");
         }
 
         static function find($search_id)
@@ -65,36 +65,44 @@
             return $found_store;
         }
 
-        function updateStore($edit_name)
+        function update($edit_name)
         {
             $edit_name = ucwords(strtolower($edit_name));
             $GLOBALS['DB']->exec("UPDATE stores SET name = '{$edit_name}' WHERE id = {$this->getId()};");
             $this->setName($edit_name);
         }
 
-        function deleteStore()
+        function delete()
         {
-            $GLOBALS['DB']->exec("DELETE FROM shoes WHERE store_id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM stores_brands WHERE store_id = {$this->getId()};");
             $GLOBALS['DB']->exec("DELETE FROM stores WHERE id = {$this->getId()};");
         }
 
-        function findShoes()
+        function addBrand($new_brand)
         {
-           $returned_shoes = $GLOBALS['DB']->query("SELECT * FROM shoes WHERE store_id = {$this->getId()};");
-           $shoes = array();
-           foreach($returned_shoes as $shoe) {
-               $id = $shoe['id'];
-               $name = $shoe['name'];
-               $store_id = $shoe['store_id'];
-               $new_shoe = new Shoe($id, $name, $store_id);
-               array_push($shoes, $new_shoe);
+           $GLOBALS['DB']->exec("INSERT INTO brands_shoes (brand_id, course_id) VALUES ({$new_brand->getId()}, $this->getId()};");
+        }
+
+        function getBrands()
+        {
+           $returned_brands = $GLOBALS['DB']->query("SELECT brands *. FROM stores
+               JOIN stores_brands ON (stores_brands.store_id = stores.id)
+               JOIN brands ON (brand.id = stores_brands.brand_id)
+               WHERE brands.id = {$this->getId()};");
+           $brands = array();
+           foreach($returned_brands as $brand) {
+               $id = $brand['id'];
+               $name = $brand['name'];
+               $store_id = $brand['store_id'];
+               $new_brand = new Brand($id, $name);
+               array_push($brands, $new_brand);
            }
-           return $shoes;
+           return $brands;
        }
 
-       function deleteShoes()
+       function deleteBrands()
        {
-          $returned_shoes = $GLOBALS['DB']->query("DELETE FROM shoes WHERE store_id = {$this->getId()};");
+          $GLOBALS['DB']->exec("DELETE FROM brands_shoes WHERE store_id = {$this->getId()};");
        }
 
     }
