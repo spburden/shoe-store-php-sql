ddaa4d1ef57bf5638eb3be82ebf2246bbb739658
Stephen Burden <spburden@hotmail.com>
2016-09-30 11:00:55 -0700

    add store test for deleting stores brands

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -151,7 +151,31 @@
 
             //Assert
             $this->assertEquals([$test_brand], $output);
+        }
 
+        function test_deleteBrands()
+        {
+            //Arrange
+            $store_name = "Foot Locker";
+            $test_store = new Store($store_name);
+            $test_store->save();
+
+            $brand_name1 = "Nike";
+            $test_brand1 = new Brand($brand_name1);
+            $test_brand1->save();
+            $test_store->addBrand($test_brand1);
+
+            $brand_name2 = "Adidas";
+            $test_brand2 = new Brand($brand_name2);
+            $test_brand2->save();
+            $test_store->addBrand($test_brand2);
+
+            //Act
+            $test_store->deleteBrands();
+            $output = $test_store->getBrands();
+
+            //Assert
+            $this->assertEquals([], $output);
         }
 
 
