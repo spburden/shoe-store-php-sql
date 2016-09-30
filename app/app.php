f2d29779950d8ff13dec5aadff9ee763b0c62ede
Stephen Burden <spburden@hotmail.com>
2016-09-30 11:41:30 -0700

    add basic index twig page and app routing

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Brand.php";
-    require_once __DIR__."/../src/Shoe.php";
+    require_once __DIR__."/../src/Store.php";
 
     date_default_timezone_set('America/Los_Angeles');
 
@@ -16,8 +16,8 @@
     $app['debug'] = true;
 
     // //ALTERNATIVE SERVER:
-    $server = 'mysql:host=localhost;dbname=shoe_store';
-    // $server = 'mysql:host=localhost:8889;dbname=shoe_store';
+    //$server = 'mysql:host=localhost;dbname=shoes';
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -30,7 +30,32 @@
 
     $app->get("/", function() use ($app) {
         $brands = Brand::getAll();
-        return $app['twig']->render('index.html.twig', array('brands' => $brands));
+        $stores = Store::getAll();
+        return $app['twig']->render('index.html.twig', array('brands' => $brands, 'stores' => $stores));
+    });
+
+    $app->post("/add_brand", function() use ($app) {
+          $new_brand = $_POST['new_brand'];
+          $new_brand = new Brand($new_brand);
+          $new_brand->save();
+          return $app->redirect("/");
+    });
+
+    $app->post("/add_store", function() use ($app) {
+          $new_store = $_POST['new_store'];
+          $new_store = new Store($new_store);
+          $new_store->save();
+          return $app->redirect("/");
+    });
+
+    $app->post("/delete_brands", function() use ($app) {
+          Brand::deleteAll();
+          return $app->redirect("/");
+    });
+
+    $app->post("/delete_stores", function() use ($app) {
+          Store::deleteAll();
+          return $app->redirect("/");
     });
 
     return $app;
